1c7f4842a8ac991c1aa75d2529c7785f4ce941e5
Luis Valdovinos <lvaldovinos@foundationmedicine.com>
2015-03-25 12:28:59 -0600

    Send multiple types support

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 'use strict';
 
+var _util = {
+  isObject : function(obj) {
+    var type = typeof obj;
+    return type === 'function' || type === 'object' && !!obj && !(this.isArray(obj)) ;
+  },
+  isArray : function(obj) {
+    return toString.call(obj) === '[object Array]';
+  },
+  isDate : function(obj) {
+    return toString.call(obj) === '[object Date]';
+  }
+};
+
 module.exports = function(express) {
   var res;
   if (express && express.Router) {
     if (!express.wr) {
       express.wr = function(res, spec) {
+        var spec = spec || '';
         var code = spec.code || 200,
-            data = (typeof spec === 'string') ? spec : spec.data || '',
+            data = !(_util.isObject(spec)) ? (_util.isDate(spec) ? spec.toString() : spec) : spec.data,
             message = spec.message || '',
             status = (code >= 500 && code < 600) ? 'fail'
                      : (code >= 400 && code < 500) ? 'error' : 'success' ;
